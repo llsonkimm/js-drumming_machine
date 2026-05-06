74feccb621ac54773a274fac1987c93b6bb61e76
llsonkimm <lsoncheruiyot@gmail.com>
2026-05-06 09:34:40 +0300

    Updated index and JS files

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".drum-pad");
 const audioElements = document.querySelectorAll("audio");
+const sounds = {
+    'Q': 'Heater 1',
+    'W': 'Heater 2',
+    'E': 'Heater 3',
+    'A': 'Heater 4',
+    'S': 'Clap',
+    'D': 'Open-HH',
+    'Z': 'Kick-n-Hat',
+    'X': 'Kick',
+    'C': 'Closed-HH'
+};
 
-const audio = new Audio();
 
 const playSound = (sound) => {
-    audio.currentTime = 0;
-    audio.src = sound;
-    audio.play().catch(error => console.log(error));
-}
-
-const playKey = (key) => {
-  const button = document.getElementById(key).parentElement;
+    sound.currentTime = 0;
+    const playPromise = sound.play();
+    
+    if (playPromise !== undefined) {
+        playPromise.catch(error => console.log(error));
+    }
   
+  
+  display.textContent = sounds[sound.id] || sound.id;
+  
+  
+  const button = sound.parentElement;
   button.classList.add("active");
   
   setTimeout(() => {
     button.classList.remove("active")
   }, 100)
-} 
+}
 
-
-
-window.addEventListener("keydown", (event) => {
+document.addEventListener("keydown", (event) => {
   const key = event.key.toUpperCase();
   const audio = document.getElementById(key);
   
-  if(audio) {
-    audio.currentTime = 0;
-    audio.play().catch(error => console.log(error));
-    display.textContent = audio.innerText
-    playKey(key)
-  }
+if (audio && audio.classList.contains('clip')) {
+        playSound(audio);
+    }
   })
    
 
 buttons.forEach(button => button.addEventListener("click", () => {
-  const audioFile = button.firstElementChild;
-  const audioSrc = audioFile.src;
-  display.textContent = audioFile.innerText;
-  playSound(audioSrc)
+  const audio = button.querySelector(".clip");
+  playSound(audio);
 }))
